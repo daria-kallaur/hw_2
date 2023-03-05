bd77ce5c6d297abbe1aaacf1e9b9c66ff4a58e29
Daria Kallaur <kallaur.daria@gmail.com>
2023-03-05 16:11:47 +0100

    Add existing file

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = +prompt("Enter your first number", 2);
+let secondNumber = +prompt("Enter your second number", 2);
+
+let result = firstNumber + secondNumber;
+let result2 = firstNumber - secondNumber;
+let result3 = firstNumber * secondNumber;
+let result4 = firstNumber / secondNumber;
+
+alert(`${firstNumber} + ${secondNumber} = ${result}
+${firstNumber} - ${secondNumber} = ${result2}
+${firstNumber} * ${secondNumber} = ${result3}
+${firstNumber} / ${secondNumber} = ${result4}`);
